9c5447dd29f4df267323fe155965a40260c9bf27
vikash sharma <vk220783@gmail.com>
2026-03-16 12:54:01 +0530

    learning mouse event

--- nareshItReact/shangviMam/testing/src/components/card/card.component.jsx
+++ nareshItReact/shangviMam/testing/src/components/card/card.component.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
 
 function CardComponent() {
-  const [product, setProduct] = useState({
-    title: "",
-    rating: {
-      rate: 0,
-      review: 0,
+  const [color, setColor] = useState("red");
+
+  const [product, setProduct] = useState([
+    {
+      title: "",
+      rating: {
+        rate: 0,
+        review: 0,
+      },
+      price: 0,
+      offers: [],
+      image: "",
     },
-    price: 0,
-    offers: [],
-    image: "",
-  });
+  ]);
 
   function getData() {
     const http = new XMLHttpRequest();
@@ -28,45 +32,64 @@ function CardComponent() {
     getData();
   }, []);
 
+  // function randomNumber() {
+  //   let r = Math.floor(Math.random() * 255);
+  //   let g = Math.floor(Math.random() * 255);
+  //   let b = Math.floor(Math.random() * 255);
+  //   return `rgb(${r},${g},${b})`;
+  // }
+  // const handelChangeColor = () => {
+  //   setColor(randomNumber());
+  // };
   return (
     <>
-      <div className="container mt-5 w-50 border p-5 shadow">
-        <div className="row">
-          <div className="col-3">
-            <img src={product.image} className="img-fluid " />
-          </div>
-          <div className="col-9">
-            <h3 className="text-black p-2">
-              {product?.title}
-            </h3>
-            <p className="">
-              <span className="bi bi-star-fill m-2 badge bg-success"> {product?.rating?.rate}</span>
-            </p>
+      {product.map((product) => {
+        return (
+          <div className="container mt-5 w-50 border p-5 shadow">
+            <div className="row " >
+              <div className="col-3">
+                <img src={product.image} className="img-fluid " />
+              </div>
+              <div className="col-9">
+                <h3 className="text-black p-2">{product?.title}</h3>
+                <p className="">
+                  <span className="bi bi-star-fill m-2 badge bg-success">
+                    {" "}
+                    {product?.rating?.rate}
+                  </span>
+                </p>
 
-            <p className="bi bi-star-fill m-2 badge bg-success text-warning"> {product?.rating?.review}</p>
-            <p>
-              {product?.price.toLocaleString("en-in", {
-                style: "currency",
-                currency: "INR",
-              })}
-            </p>
+                <p className="bi bi-star-fill m-2 badge bg-success text-warning">
+                  {" "}
+                  {product?.rating?.review}
+                </p>
+                <p>
+                  {product?.price.toLocaleString("en-in", {
+                    style: "currency",
+                    currency: "INR",
+                  })}
+                </p>
 
-            {product?.offers.map((items, index) => (
-              <li
-                key={index}
-                className="list-unstyled bi bi-tag-fill text-success"
-              >
-                <span
-                  className="text-black
+                {/* <button onClick={handelChangeColor}>change</button> */}
+
+                {product?.offers.map((items, index) => (
+                  <li
+                    key={index}
+                    className="list-unstyled bi bi-tag-fill text-success"
+                  >
+                    <span
+                      className="text-black
              "
-                >
-                   {items}
-                </span>
-              </li>
-            ))}
+                    >
+                      {items}
+                    </span>
+                  </li>
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </>
   );
 }
---
+++ nareshItReact/shangviMam/testing/src/components/demo/demo.component.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+function DemoComponent() {
+  //  defining the state
+
+  const [styleObj, setStyleObj] = useState({
+    color: "black",
+  });
+
+  const handleMouseOver = (e) => {
+    setStyleObj({
+      color: e.target.id,
+    });
+  };
+
+  const handelMouseOut = (e) => {
+    setStyleObj({
+      color: "black",
+    });
+  };
+  return (
+    <>
+      <div className="container-fluid">
+        <div
+          onMouseOver={handleMouseOver}
+          id="red"
+          style={{ backgroundColor: "red" }}
+        >
+          red
+        </div>
+        <div
+          onMouseOver={handleMouseOver}
+          style={{ backgroundColor: "blue" }}
+          id="blue"
+        >
+          blue
+        </div>
+        <div
+          onMouseOver={handleMouseOver}
+          id="green"
+          style={{ backgroundColor: "green" }}
+        >
+          green
+        </div>
+
+        <h1 style={styleObj} onMouseOut={handelMouseOut}>
+          sample text
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default DemoComponent;
