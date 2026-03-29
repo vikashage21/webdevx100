9f7e2da0ae77bc373fa04376f5bdf641d6228ca5
vikash sharma <vk220783@gmail.com>
2026-03-29 21:19:11 +0530

    given number is odd or not

---
+++ dsa/Dsa_sanket/questions/question1.js
@@ -0,0 +1,14 @@
+// question : - given a number x , check if the number is even or odd ??
+
+
+function isEvenOdd(num){
+    if(num%2 === 0){
+        return "Even"
+    }else{
+        return "Odd"
+    }
+}
+
+
+
+console.log(isEvenOdd(10))
